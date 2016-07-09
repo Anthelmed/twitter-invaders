4509075347a7e171f5096523ab2661d8b3378fdc
anthelme_dumont <anthelme.dumont@gmail.com>
2016-07-09 03:47:33 +0200

    front receive data (tweet) from socket io

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 /*eslint-env node*/
 import express from 'express';
 import Twit from 'twit';
+import http from 'http';
 import webpack from 'webpack';
 import history from 'connect-history-api-fallback';
 import portNumber from 'port-number';
@@ -10,10 +11,34 @@ import devConfig from './webpack/webpack.dev.config.babel.js';
 
 const env = process.env.NODE_ENV;
 const isDeveloping = env !== 'production';
-const port = process.env.PORT ? process.env.PORT : portNumber();
-const ip = isDeveloping ? '127.0.0.1' : '127.0.0.1';
+const port = isDeveloping ? 9000 : process.env.PORT;
+const ip = '127.0.0.1';
 
 const app = express();
+
+//Socket io
+const server = new http.Server(app);
+const io = require('socket.io')(server);
+
+//Twitter Api
+const T = new Twit({
+    consumer_key:         'efrF4jmviXP13UdR0FhroUw9u',
+    consumer_secret:      'E1gwGuEcB1AyDc5WkowXpH3LSiAFxHGpB18y4t0tDU7C5nnSJu',
+    access_token:         '725371147324391429-iuVmH8ok7idKYYGEGhs2dW0mOXTNbYb',
+    access_token_secret:  'Sb13X2FiHCBTjwx8UC6yfzhWTxXZyypxkfm8Fdea8WvV9',
+    timeout_ms:           60*1000  // optional HTTP request timeout to apply to all requests.
+});
+
+//Stream using twitter tracks and Socket io
+var stream = T.stream('statuses/filter', { track: '#apple' });
+
+io.on('connection', (socket) => {
+    stream.on('tweet', function (tweet) {
+        socket.emit('tweet', tweet);
+    });
+});
+
+server.listen(3000);
 
 app.use( history() );
 
@@ -24,6 +49,10 @@ if( isDeveloping ) {
     app.use( webpackDevMiddleware( compiler, {
         publicPath: devConfig.output.publicPath,
         headers: { 'Access-Control-Allow-Origin': '*' },
+        hot: false,
+        noInfo: true,
+        quiet: false,
+        proxy: { '*': 'http://127.0.0.1:3000' },
         stats: {
             colors: true,
             hash: false,
@@ -50,19 +79,4 @@ app.listen( port, ip, error => {
     /*eslint-disable no-console */
     console.info( `[${env}] Listening on port ${port}. Open up http://${ip}:${port}/ in your browser.` );
     /*eslint-enable no-console */
-});
-
-//Twitter Api
-const T = new Twit({
-    consumer_key:         'efrF4jmviXP13UdR0FhroUw9u',
-    consumer_secret:      'E1gwGuEcB1AyDc5WkowXpH3LSiAFxHGpB18y4t0tDU7C5nnSJu',
-    access_token:         '725371147324391429-iuVmH8ok7idKYYGEGhs2dW0mOXTNbYb',
-    access_token_secret:  'Sb13X2FiHCBTjwx8UC6yfzhWTxXZyypxkfm8Fdea8WvV9',
-    timeout_ms:           60*1000  // optional HTTP request timeout to apply to all requests.
-});
-
-var stream = T.stream('statuses/filter', { track: '#apple' });
-
-stream.on('tweet', function (tweet) {
-    console.log(tweet);
 });
--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+import io from 'socket.io-client';
+const socket = io.connect('127.0.0.1:3000', { reconnect: true });
+
+socket.on('tweet', function(tweet) {
+    console.log(tweet);
+});
